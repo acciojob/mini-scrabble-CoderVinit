2fda49ed24a4bc9f774078911924af99ced3d095
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-03-10 11:27:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let h2 = document.getElementById("letterCount");
+
+document.getElementById("evaluatedText").oninput = function(){
+	let val = this.value;
+
+	if(val === ""){
+		h2.textContent = 0
+	}
+
+	h2.textContent = val.length;
+}
